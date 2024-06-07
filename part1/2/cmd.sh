b740a926112494d26d94cbbe33433044331ecb5e
domodomodomo <34803041+domodomodomo@users.noreply.github.com>
2024-06-07 12:46:56 +0900

    Update cmd.sh

--- part1/2/cmd.sh
+++ part1/2/cmd.sh
@@ -1,6 +1,7 @@
-export BLUE="Squirtle"
+
 
 docker image build --tag app .
 
 # Only define the environment variable RED when running the container.
+export BLUE="Squirtle"
 docker container run --env RED="Charmander" app 
