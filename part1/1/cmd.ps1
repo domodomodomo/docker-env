8c8ed8aec670cc4c694a80e32d5e4b067f411611
zundamon <zundamon@example.com>
2024-06-12 02:53:02 +0900

    -

--- part1/1/cmd.ps1
+++ part1/1/cmd.ps1
@@ -1,7 +1,10 @@
 
+
+
 docker image build `
   --no-cache `
   --progress=plain `
+  --build-arg BLUE="Squirtle" `
   --tag app `
   .
 docker container run app
--- part1/1/cmd.sh
+++ part1/1/cmd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
 docker image build \
   --no-cache \
   --progress=plain \
+  --build-arg BLUE="Squirtle" \
   --tag app \
   .
 docker container run app
