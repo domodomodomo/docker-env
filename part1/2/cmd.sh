85ac74fe7330e87b2d217c9071f26281a848a4d0
zundamon <zundamon@example.com>
2024-06-16 07:31:59 +0900

    -

---
+++ part1/2/cmd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+docker image build \
+    --tag app \
+    --progress=plain \
+    --no-cache \
+    .
+
+docker image inspect app \
+    --format '{{range .Config.Env}}{{println .}}{{end}}'
+
+docker container run \
+    --env BLUE=Bulbasaur \
+    --env-file .env.local \
+    --name app \
+    app
+
+docker container inspect app \
+    --format '{{range .Config.Env}}{{println .}}{{end}}'
+
+docker container rm app
